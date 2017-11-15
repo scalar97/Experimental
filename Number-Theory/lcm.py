f58f356fe765ec2910b0b1a8e5619db8b6b66b93
tati-z <tatianazihindula@gmail.com>
2017-11-15 15:53:18 +0000

    renamed the file to is_prime and fixed unittest errors

--- Number-Theory/lcm.py
+++ Number-Theory/lcm.py
@@ -7,7 +7,7 @@ from numpy import prod
 def lcm_gcd(*arg):
 	if len(arg) > 2:
 		num1 = prod(arg[: len(arg) -1])
-		num2 = arg[:-1]
-		return int(num1 * num2 / euclidian_gcd(num1,num2))
+		num2 = arg[len(arg)-1]
+		return abs(int(num1 * num2 / euclidian_gcd(num1,num2)))
 	else:
-		return int(arg[0]*arg[1] /euclidian_gcd(arg[0],arg[1]))
+		return abs(int(arg[0]*arg[1] /euclidian_gcd(arg[0],arg[1])))
