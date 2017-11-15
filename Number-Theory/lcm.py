9b31b83663882bc62319291bd5630957076810a5
tati-z <tatianazihindula@gmail.com>
2017-11-15 15:22:56 +0000

    uses the numpy prod function for speed

--- Number-Theory/lcm.py
+++ Number-Theory/lcm.py
@@ -1,8 +1,13 @@
 # function to find the lowest common of a number using the gcd of the two numbers
 
 from gcd import euclidian_gcd
+from numpy import prod
 
-def lcm_gcd(*numbers):
-	n = number[: len(numbers) -1]
-	product = int(reduce(lambda x, y: x * y, n))
-	return product * numbers[len(n)] / int(euclidian_gcd(product, numbers[len(n)]))
+
+def lcm_gcd(*arg):
+	if len(arg) > 2:
+		num1 = prod(arg[: len(arg) -1])
+		num2 = arg[:-1]
+		return int(num1 * num2 / euclidian_gcd(num1,num2))
+	else:
+		return int(arg[0]*arg[1] /euclidian_gcd(arg[0],arg[1]))
