aaf1f2bbff3ddc8728f3f19e3f8c27e631c025e0
tati-z <tatianazihindula@gmail.com>
2017-12-17 04:38:07 +0000

    renamed variable this_much_time to this_many_time

--- open_in_bowser.py
+++ open_in_bowser.py
@@ -8,13 +8,13 @@ import sys
 # to take the specified url, the time intervals in secs,and the frequency
 
 if len(sys.argv) == 4:
-	this_much_time = int(sys.argv[2])
+	this_many_time = int(sys.argv[2])
 	every_this_much_time_in_sec = int(sys.argv[3])
 	url = sys.argv[1]
-	for i in range(this_much_time):
+	for i in range(this_many_time):
 		webbrowser.open(url)
 		time.sleep(every_this_much_time_in_sec)
 else:
-	print(f'Usage:{sys.argv[0]} open_this_url  this_much_time   every_this_much_time_in_sec')
+	print(f'Usage:{sys.argv[0]} open_this_url  this_many_time   every_this_much_time_in_sec')
 	
 # example argument: python open_in_bowser.py https://google.com 2 10
