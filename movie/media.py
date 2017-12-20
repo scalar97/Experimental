2f7f73d9b36b9ee77bea581cc36dfb88fb59c933
tati-z <tatianazihindula@gmail.com>
2017-12-20 18:17:20 +0000

    changed the VALID_RATINGS class variable to a tuple

--- movie/media.py
+++ movie/media.py
@@ -1,6 +1,9 @@
+
 import webbrowser
 
 class Movie(object):
+	VALID_RATINGS = ("G", "PG", "PG-13", "R")
+	
 	def __init__(self,title, **kwargs):
 		self.title = title
 		self.storyline = kwargs.get('storyline')
