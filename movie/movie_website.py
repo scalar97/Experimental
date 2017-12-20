a23b6be8b2d9fa1bfd014c9957374e57a3bd978a
tati-z <tatianazihindula@gmail.com>
2017-12-20 14:54:46 +0000

    changed the attribute given to the ones I renames my class attributes

--- movie/movie_website.py
+++ movie/movie_website.py
@@ -135,16 +135,16 @@ def create_movie_tiles_content(movies):
 	for movie in movies:
 		# Extract the youtube ID from the url
 		youtube_id_match = re.search(
-			r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+			r'(?<=v=)[^&#]+', movie.trailer)
 		youtube_id_match = youtube_id_match or re.search(
-			r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+			r'(?<=be/)[^&#]+', movie.trailer)
 		trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
 							  else None)
 
 		# Append the tile for the movie with its content filled in
 		content += movie_tile_content.format(
 			movie_title=movie.title,
-			poster_image_url=movie.poster_image_url,
+			poster_image_url=movie.image,
 			trailer_youtube_id=trailer_youtube_id
 		)
 	return content
@@ -152,7 +152,7 @@ def create_movie_tiles_content(movies):
 
 def open_movies_page(movies):
 	# Create or overwrite the output file
-	output_file = open('fresh_tomatoes.html', 'w')
+	output_file = open('movie_website.html', 'w')
 
 	# Replace the movie tiles placeholder generated content
 	rendered_content = main_page_content.format(
