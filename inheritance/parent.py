818e47d1a1c5546bf9c6bc79b90cde103a246c65
tati-z <tatianazihindula@gmail.com>
2017-12-20 20:03:36 +0000

    now includes script output

--- inheritance/parent.py
+++ inheritance/parent.py
@@ -3,8 +3,10 @@ class Parent(object):
 		print('Parent constructor called')
 		self.last_name = last_name
 		self.eye_color = eye_color
+
+	def show_info(self):
+		return f'{self.last_name} has {self.eye_color} eyes.'
 		
-catty_billy = Parent('Billy', 'gray')
 
 class Child(Parent):
 	def __init__(self,last_name, eye_color, number_of_toys):
@@ -12,16 +14,22 @@ class Child(Parent):
 		super().__init__(last_name, eye_color)
 		self.number_of_toys = number_of_toys
 
-pat_billy = Child('Billy','blue',10)
+	def show_info(self):
+		return super().show_info()[:-1]+ f' and owns {self.number_of_toys} toys.'
 
-	
-	
+catty_billy = Parent('Billy', 'gray')
+pat_billy = Child('Billy','brown',10)
 
-
-
-
-
-
+print(catty_billy.show_info())
+print(pat_billy.show_info())
 
 
+''' +SCRIPT OUTPUT
 
+	Parent constructor called
+	Child constructor called
+	Parent constructor called
+	Billy has gray eyes.
+	Billy has brown eyes and owns 10 toys.
+
+'''	
