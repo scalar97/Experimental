f5ddecf40b44ea1186762779fc7296315147c628
tati-z <tatianazihindula@gmail.com>
2018-01-02 03:41:53 +0000

    the max_ bound is no longer included in the range

--- Number-Theory/prime_upto_n.py
+++ Number-Theory/prime_upto_n.py
@@ -1,8 +1,8 @@
 def prime_upto_n(max_):
 	prime_divisors = set()
 	primes = set()
-	
-	for prime in range(2,max_ + 1):
+
+	for prime in range(2,max_):
 		if prime not in prime_divisors:
 			j = prime * 2 # start counting from next prime factor
 			while j <= max_: # stay in the bouds specified by max_
