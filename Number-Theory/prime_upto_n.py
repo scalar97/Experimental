a72760414472f777117e8c3a3b36ee45b667d067
tati-z <tatianazihindula@gmail.com>
2018-01-02 03:24:09 +0000

    now uses the set to filter previously seen primes, also j incremenents largely

--- Number-Theory/prime_upto_n.py
+++ Number-Theory/prime_upto_n.py
@@ -1,16 +1,14 @@
 def prime_upto_n(max_):
-	all_numbers = [ x for x in range(2,max_) ]
+	prime_divisors = set()
 	primes = set()
-	i = 0
-	while i < len(all_numbers):
-		prime = all_numbers.pop(0)
-		j = 0
-		while j < len(all_numbers):
-			if all_numbers[j] % prime == 0:
-				del all_numbers[j]
-			j += 1
-		primes.add(prime)
-		i += 1
-	primes.update(all_numbers)
-
+	
+	for prime in range(2,max_ + 1):
+		if prime not in prime_divisors:
+			j = prime * 2
+			while j <= max_:
+				if j % prime == 0:
+					prime_divisors.add(j)
+				j += prime
+			primes.add(prime)
+		
 	return primes
