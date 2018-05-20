47f2f1a68a81bdbfd97166551c4c9e56344ec998
Tatiana Zihindula <tati@Tatianas-MacBook-Pro.local>
2018-05-20 21:42:44 +0100

    the view now updates accordingly and fixed stroke bug linked to stroke width

--- Flow/Flow/ViewController.swift
+++ Flow/Flow/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var drinkProgress: progressCounter!
+    @IBOutlet weak var drunkCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func drinkActions(_ sender: PushButton) {
+        if sender.isAddButton {
+            drinkProgress.didDrink()
+        } else {
+            drinkProgress.didLie()
+        }
+        drunkCount.text = "\(drinkProgress.getGlassesDrunk())"
+    }
 }
 
