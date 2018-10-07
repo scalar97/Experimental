5498ab6f6bbdb08fabb4ee84c9bf6b347cfb9c14
Tatiana Zihindula <tatianazihindula@gmail.com>
2018-10-07 14:39:59 +0100

    found the favourable date to invite partners from with local data 🤷🏽‍♀️

--- python/invitation/parteners.py
+++ python/invitation/parteners.py
@@ -1,63 +1,65 @@
-import urllib.request
+# Author : Tatiana Zihindula ~ 7/10/2018
+
 import json
 import dateutil.parser
-import requests
 
-class Partener(object):
-	country = ""
-	attendee_count = 0
-	attendees = set()
-	start_date = ""
+class Partner(object):
+	# class method to keep track of favourable dates
+	def __init__(self):
+		self.country = ""
+		self.attendee_count = 0
+		self.start_date = ""
+		self.attendees = set()
+		self.favour = dict()
 
-	# get the suitable date for each country
-	# the best way would be to use a heap but I will use a dictionary as time is short
-	def getDate(self):
-		favour = dict()
-		for a in self.attendees:
-			dates = a['availableDates']
-			for i in range(len(dates) - 1):
-				curr_date = dateutil.parser.parse(dates[i])
-				next_date = dateutil.parser.parse(dates[i+1])
-				delta = next_date - curr_date
+	# the best way would be to use a heap as it will self sort in (O)log n
+	# this would allow to merge update_date & set_startdate methods in 1 method.
+	# but I will use a dictionary as time is short.
+	def update_date(self, dates):
+		for i in range(len(dates) - 1):
+			curr_date = dateutil.parser.parse(dates[i])
+			next_date = dateutil.parser.parse(dates[i+1])
+			delta = next_date - curr_date
 
-				# only add successive days; don't compute values we don't need
-				if delta.days == 1 :
-					if favour.get(dates[i]):
-						favour[dates[i]] += 1
-					else:
-						favour[dates[i]] = 1
+			# only add successive days; don't compute dates we won't use
+			if delta.days == 1 :
+				if dates[i] in self.favour:
+					self.favour[dates[i]] += 1
+				else:
+					self.favour[dates[i]] = 1
 
-		tempFilter = list((k, favour[k]) for k in sorted(favour, key=favour.get, reverse=True))
-		if len(tempFilter) > 0:
-			self.start_date = tempFilter[0]
-		# else : none value for start_date will be encoded to null by json
+	def set_startdate(self):
+		tmp = [(k, self.favour[k]) for k in sorted(self.favour, key=self.favour.get, reverse=True)]
+		if len(tmp) > 0:
+			self.start_date = tmp[0][0]
+		else :
+			self.start_date = 'No favourable starting date'
+
+	def __repr__(self):
+		display = self.__dict__ ; display.pop('favour')	# ignore the favour dict
+		return "\n\n%r" % (display)
 
 if __name__ == '__main__':
-	# get the list of parteners
-	weburl = urllib.request.urlopen('get_url_goes_here')
-	data = weburl.read()
-	encoding = weburl.info().get_content_charset('utf-8')
-	json_data = json.loads(data.decode(encoding))
+	# get the list of partners
+	with open('entries.json') as f:
+		data = json.load(f)
+		partners = data['partners']
 
-	parteners = json_data['parteners']
+		# create a dictionary entry for each country
+		countries = dict()
+		for item in partners:
+			c = item['country']
+			if not c in countries:
+				# create a new entry for this new country
+				p = Partner()
+				p.country = c
+				countries[c] = p
+			# update the country data whether with existant or newly created country.
+			countries[c].attendee_count += 1
+			countries[c].attendees.add(item['email'])
+			countries[c].update_date(item['availableDates'])
 
-	# create a dictionary of countries
-	countries = dict()
-	for item in parteners :
-		c = item['country']
-		# create a new entry for new country
-		if not countries.get(c):
-			p = Partener()
-			p.country = c
-			countries[c] = p
-		# update the country data whether existant or newly created country
-		countries[c].attendee_count += 1
-		countries[c].attendees.add(item['email'])
-
-	for aPartener in countries:
-		# now get varourable dates for each all partener
-		aPartener.getDate()
-
-	# send the favourable date back as post
-	resolved = json.dumps(countries).encode('utf8')
-	requests.post("post_url_goes_here", json=resolved)
+		for country in countries:
+			# finally set favourable dates for Parterners from each country.
+			countries[country].set_startdate()
+			print(countries[country])
