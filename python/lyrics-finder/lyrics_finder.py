ae315cc7c630382f96d00148725084a71472c4b9
Tatiana Zihindula <rickless137@gmail.com>
2018-10-26 15:01:11 +0100

    now error tolerant as it no longer builds the url + new split algorithm

--- python/lyrics-finder/lyrics_finder.py
+++ python/lyrics-finder/lyrics_finder.py
@@ -1,36 +1,53 @@
 import re
-import urllib.request
-from bs4 import BeautifulSoup
 import sys
+import requests
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
 
-def get_lyrics(song_title,artist):
-	# remove all except alphanumeric characters from artist and song_title
-	artist = re.sub('([^A-Za-z0-9]+|^the)', '', artist.lower())
-	song_title = re.sub('[^A-Za-z0-9]+', '', song_title.lower())
-	url = 'https://azlyrics.com/lyrics/'+artist+'/'+song_title+'.html'
+def print_error(err = '', usage = False):
+	if usage:
+		err = 'Usage: '+ sys.argv[0]+' followed by song title'
+	else:
+		err = 'The following error occured :\n' + err
+	print(err,file= sys.stderr)
+	exit(-1)
+
+def get_lyrics(url):
 	try:
 		response = urllib.request.urlopen(url).read()
 		soup = BeautifulSoup(response, 'html.parser')
-		lyrics = str(soup)
+		l = str(soup)
 		# lyrics lies between up_partition and down_partition
-		up_partition = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
-		down_partition = '<!-- MxM banner -->'
-		lyrics = lyrics.split(up_partition)[1].split(down_partition)[0]
+		start = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
+		end = '<!-- MxM banner -->'
+		lyrics = l[l.find(start)+len(start):l.rfind(end)]
 		# remove html tags
 		lyrics = re.sub('<\s*\w[^>]*>|</\w*>','', lyrics)
 		return lyrics
 	
 	except Exception as e:
-		return 'The following error occurred\n' + url + '\n'+str(e)
+		err = url + '\n'+str(e)
+		print_error(err = err)
 
 if __name__ == '__main__':
-	args = []
-	if len(sys.argv) == 3:
-		args = sys.argv[1:]
-	else:
-		args = ''.join(sys.argv[1:]).split('-')
-	assert len(args) == 2 , 'Error:invalid arguments:\nExpected: song tite - artist name\nReceived: '+ ' '.join(sys.argv[1:])
-
-	print(args[0].title(),' -', args[1].title())
-	print(get_lyrics(*args))
-	
+	try:
+		if len(sys.argv) < 2:
+			print_error(usage=True)
+		r = requests.get('https://duckduckgo.com/html/?q='+'+'.join(sys.argv[1:])+'+lyrics')
+		soup = BeautifulSoup(r.text, 'html.parser')
+		print('- ' + ' '.join(sys.argv[1:]))
+		results = soup.find_all('a', attrs={'class':'result__a'}, href=True, limit=7)
+		url = None
+		for tag in results:
+			parsed = urllib.parse.urlparse(tag['href'])
+			link = urllib.parse.parse_qs(parsed.query)['uddg'][0]
+			if link.startswith('https://www.azlyrics.com'):
+				url = link
+				break
+		if url:	
+			print(get_lyrics(url))
+		else:
+			print_error(err= 'No fast result found')
+	except Exception as e:
+		print_error(err= str(e))
