47edb0aca8180d39b348aee6bccb65f414a95842
Tatiana Zihindula <rickless137@gmail.com>
2018-10-26 22:10:39 +0100

    optimised slower parts for speed 🚀👻🎸

--- python/lyrics-finder/lyrics_finder.py
+++ python/lyrics-finder/lyrics_finder.py
@@ -1,53 +1,42 @@
 import re
 import sys
-import requests
 import urllib.request
 import urllib.parse
-from bs4 import BeautifulSoup
-
-def print_error(err = '', usage = False):
-	if usage:
-		err = 'Usage: '+ sys.argv[0]+' followed by song title'
-	else:
-		err = 'The following error occured :\n' + err
-	print(err,file= sys.stderr)
-	exit(-1)
+from bs4 import BeautifulSoup,SoupStrainer
 
 def get_lyrics(url):
 	try:
 		response = urllib.request.urlopen(url).read()
 		soup = BeautifulSoup(response, 'html.parser')
 		l = str(soup)
-		# lyrics lies between up_partition and down_partition
-		start = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
-		end = '<!-- MxM banner -->'
-		lyrics = l[l.find(start)+len(start):l.rfind(end)]
-		# remove html tags
-		lyrics = re.sub('<\s*\w[^>]*>|</\w*>','', lyrics)
-		return lyrics
-	
-	except Exception as e:
-		err = url + '\n'+str(e)
-		print_error(err = err)
+		# lyrics lies between 'l_start' and '<!-- MxM banner -->'
+		l_start = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
+		start = l.find(l_start,4096, len(l)) # Move away 4096 bytes as lyrics start far down the page aroud 6000+ bytes on average.
+		end = l.find('<!-- MxM banner -->', start+len(l_start))
+
+		lyrics = soup.title.string.split('Lyrics', 1)[0] + '\n' + l[start + len(l_start):end]
+		return BeautifulSoup(lyrics, 'html.parser').get_text()
+	except:
+		raise
 
 if __name__ == '__main__':
 	try:
 		if len(sys.argv) < 2:
-			print_error(usage=True)
-		r = requests.get('https://duckduckgo.com/html/?q='+'+'.join(sys.argv[1:])+'+lyrics')
-		soup = BeautifulSoup(r.text, 'html.parser')
-		print('- ' + ' '.join(sys.argv[1:]))
-		results = soup.find_all('a', attrs={'class':'result__a'}, href=True, limit=7)
-		url = None
-		for tag in results:
-			parsed = urllib.parse.urlparse(tag['href'])
-			link = urllib.parse.parse_qs(parsed.query)['uddg'][0]
-			if link.startswith('https://www.azlyrics.com'):
-				url = link
-				break
-		if url:	
-			print(get_lyrics(url))
+			print('Usage: '+ sys.argv[0]+' followed by song title', file= sys.stderr)
 		else:
-			print_error(err= 'No fast result found')
-	except Exception as e:
-		print_error(err= str(e))
+			# getting the first 7 links from DuckDuckGo search engine
+			res = urllib.request.urlopen('https://duckduckgo.com/html/?q='+'+'.join(sys.argv[1:])+'+lyrics').read()
+			soup = BeautifulSoup(res,'html.parser', parse_only=SoupStrainer('a',{'class': 'result__snippet'}))
+			results = soup.find_all('a', limit=7)
+			url = None
+			for tag in results:
+				parsed = urllib.parse.urlparse(tag['href'])
+				url = urllib.parse.parse_qs(parsed.query)['uddg'][0]
+				if url.startswith('https://www.azlyrics.com'):
+					break
+			if url:
+				print(get_lyrics(url))
+			else:
+				print('Sorry, No fast results found!\n', file=sys.stderr)
+	except:
+		raise
