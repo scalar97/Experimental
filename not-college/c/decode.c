62498bc24878665a4ca5616d6f01aa6b777ade58
tati <rickless137@gmail.com>
2018-12-13 17:03:17 +0000

    moved freopen after forloop 'just in case'?

--- not-college/c/decode.c
+++ not-college/c/decode.c
@@ -1,36 +1,37 @@
-/* simple xor cipher to encrypt a file using a password
+/* simple c program that uses xor cipher to encrypt/decrypt a file with a password
+ * e.g ./prog_name password ./secret_file
  */
 
 #include<stdio.h>
 #include<stdlib.h>
 
-enum { PASSWORD = 1, IN_FILE, ARG_NUM};
+enum {PROG_NAME, PASSWORD, INPUT_FILE, ARG_NUM};
 
-void usage(char*exe)
-{printf("Usage: %s password finename\n", exe);}
+void usage(char* prog_name)
+{printf("\nUsage: %s password finename\n\n", prog_name);}
 
 int main(int argc, char **argv){
     FILE *fp;
     char *password;
     
     if (argc == ARG_NUM && (password = argv[PASSWORD]) ) {
-	if((fp = fopen(argv[IN_FILE], "rb")) != NULL) {
+	if((fp = fopen(argv[INPUT_FILE], "rb")) != NULL) {
 	    fseek(fp, 0L, SEEK_END);
 	    const long SIZE = ftell(fp);
 	    char *buffer = calloc(SIZE, 1);
 	    rewind(fp);
 	    fread(buffer, 1, SIZE, fp);
-	    fp = freopen(argv[IN_FILE], "wb", fp);
 	    for(long i = 0; i < SIZE; i++){
 		if (!*password) password = argv[PASSWORD];
 		buffer[i] ^= *(password++);
 	    }
+	    fp = freopen(argv[INPUT_FILE], "wb", fp);
 	    fwrite(buffer, 1, SIZE, fp);
 	    fclose(fp);
 	    free(buffer);
 	    return 0;
 	}
     }
-    usage(argv[0]);
+    usage(argv[PROG_NAME]);
     return 1;
 }
